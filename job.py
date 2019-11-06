4ff93482a42c7fe0439a1afae85c08d7cbca2738
Lauryn Gomez <lauryn@cmsllcnw.com>
2019-11-06 13:23:58 -0800

    passed default attributes test

--- job.py
+++ job.py
@@ -1,7 +1,9 @@
 # Job: A process or task that has a priority.
 # Your implementation should pass the tests in test_job.py.
-# YOUR NAME
+# LAURYN GOMEZ
 
 class Job:
 
-    pass
+    def __init__(self):
+        self.priority = None
+        self.message = None
